b773f9979385001c0fd365f7dd0a0e4fc2f84052
Desi Nurhidayati <desi.nurhidayati94@gmail.com>
2019-03-27 17:00:26 +0700

    tambah rincian saldo pembayaran

--- views/smartmeter/topup.php
+++ views/smartmeter/topup.php
@@ -3,10 +3,8 @@
 use app\widgets\Button;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\helpers\Url;
 
 $this->title = 'Top Up Smart Meter';
-
 ?>
 <style>
     .btn1 {
@@ -28,64 +26,85 @@ $this->title = 'Top Up Smart Meter';
         background-color: #21BC12;
         color: white;
     }
+
+    .item{
+        grid-area: myArea;
+    }
+
+    .grid-container{
+        display: grid;
+        grid-gap: 10px;
+        grid-template-areas: 'myArea myArea';
+        background-color: #E7E7E7;
+        padding: 5px;
+    }
 </style>
 
-<div class="row">
-    <div class="col-md-12">
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <h3 class="box-title">&nbsp;</h3>
-                <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                    </button>
+<div class ="smart-meter-top-up">
+    <div class="container">
+        <div class = "col-md-6">
+            <br/>
+            <div class="row">
+                <?php $form = ActiveForm::begin();
+
+                    $topup->unitid = '107 P Permata Hijau';
+                    echo $form->field($topup, 'unitid')->textInput(['disabled' => 'disabled'])->label('Unit Apartemen');
+                ?>
+            </div>
+            <div class="row">
+                    <label>Harga</label>
+                    <br/>
+                    <!-- Button Ongoing  -->
+                    <?= Html::buttonInput('IDR 100,000', ['value' => 64, 'name' => 'a', 'class' => 'btn1 btn-success']) ?>
+
+                    <?= Html::buttonInput('IDR 200,000', ['value' => 128, 'name' => 'a', 'class' => 'btn1 btn-success']) ?>
+
+                    <?= Html::buttonInput('IDR 300,000', ['value' => 192, 'name' => 'a','class' => 'btn1 btn-success']) ?>
+            </div>
+            <div class="row">
+                <?= Html::buttonInput('IDR 500,000', ['value' => 320, 'name' => 'b','class' => 'btn1 btn-success']) ?>
+
+                <?= Html::buttonInput('IDR 1,000,000', ['value' => 640, 'name' => 'b', 'class' => 'btn1 btn-success']) ?>
+            </div>
+            <br/>
+            <div class="row">
+<!--                <label>Metode Pembayaran</label>-->
+                    <!-- <table class="table-responsive table-bordered">
+                    <tr height="10px">
+                        <td style="font-weight: bold; text-align: center">Nama</td>
+                        <td style="font-weight: bold; text-align: center">Saldo</td>
+                    </tr>
+                    <tr style="background-color: white">
+                        <td width="200px">Dana</td>
+                        <td>500,000</td>
+                    </tr>
+                    <tr>
+                        <td>GoPay</td>
+                        <td>30,000</td>
+                    </tr>
+                    <tr style="background-color: white">
+                        <td>Ovo</td>
+                        <td>0</td>
+                    </tr>
+                </table>-->
+                <div class="grid-container">
+                    <div class="item1" style="font-weight: bold; text-align: center">Metode Pembayaran</div>
+                    <div class="item2" style="font-weight: bold; text-align: center"">Saldo</div>
+                    <div class="item3"><input type="radio" name="radio" value="64"> Dana</div>
+                    <div class="item4">500,000</div>
+                    <div class="item5"><input type="radio" name="radio" value="64"> GoPay</div>
+                    <div class="item6">30,000</div>
+                    <div class="item7"><input type="radio" name="radio" value="64"> Ovo</div>
+                    <div class="item8">0</div>
                 </div>
             </div>
-            <div class="box-body">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <?= Html::label('Unit Apartemen','nama') ?>
-                        <?= Html::textInput('nama', '107 P Permata Hijau', ['id' => 'nama', 'disabled' => true, 'class' => 'form-control']) ?>
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <?= Html::label('Nominal Token','harga') ?>
-                        <div class="clearfix"></div>
-                        <?= Html::buttonInput('IDR 100,000', ['value' => 64, 'class' => 'col-xs-5 btn1 btn-success']) ?>
-                        <?= Html::buttonInput('IDR 200,000', ['value' => 128, 'class' => 'col-xs-5 btn1 btn-success']) ?>
-                        <?= Html::buttonInput('IDR 300,000', ['value' => 192, 'class' => 'col-xs-5 btn1 btn-success']) ?>
-                        <?= Html::buttonInput('IDR 500,000', ['value' => 320, 'class' => 'col-xs-5 btn1 btn-success']) ?>
-                        <?= Html::buttonInput('IDR 1,000,000', ['value' => 640, 'class' => 'col-xs-5 btn1 btn-success']) ?>
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-                <div class="col-md-6" style="margin-top: 2%">
-                    <div class="form-group">
-                        <?= Html::label('Metode Pembayaran','payment') ?>
-                        <?php //Html::radioList('payment', '', ['1' => 'Dana', '2' => 'GoPay', '3' => 'Ovo']) ?>
-                        <div class="panel panel-primary">
-                            <div class="panel-body"><input type="radio" name="metode" value="Dana"> <?= Html::label('Dana') ?><?= Html::label('100.000', 'dana', ['class' => 'pull-right']) ?></div>
-                        </div>
-                        <div class="panel panel-primary">
-                            <div class="panel-body"><input type="radio" name="metode" value="Ovo Cash"> <?= Html::label('Ovo Cash') ?><?= Html::label('200.000', 'ovo', ['class' => 'pull-right']) ?></div>
-                        </div>
-                        <div class="panel panel-primary">
-                            <div class="panel-body"><input type="radio" name="metode" value="Ovo Point"> <?= Html::label('Ovo Point') ?><?= Html::label('0', 'dana', ['class' => 'pull-right']) ?></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <?= Html::label('Total Pembayaran',null) ?><?= Html::label('IDR 105.000',null, ['class' => 'pull-right label label-warning', 'style' => 'font-size: medium']) ?>
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-                <div class="col-md-6">
-                    <a href="<?= Url::to(['smartmeter/index']) ?>" class="btn btn-block btn-primary">Beli</a>
-                </div>
+            <br/>
+            <div class="row">
+                <?= Button::create(null, 'smartmeter/index', 'btn btn-block btn-primary', 'fa fa-money', 'Top Up') ?>
             </div>
+            <br/>
+             <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
+
