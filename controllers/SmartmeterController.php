46d55492a6f31ebc64aa8eb98487587459898170
mderry3 <mderry3@gmail.com>
2019-03-27 13:47:59 +0700

    Status Smart Meter

--- controllers/SmartmeterController.php
+++ controllers/SmartmeterController.php
@@ -10,9 +10,7 @@ namespace app\controllers;
 
 use app\controllers\base\BaseController;
 use app\models\BuildingUnit;
-use app\models\Electricity;
 use Yii;
-use yii\web\ForbiddenHttpException;
 
 class SmartmeterController extends BaseController
 {
@@ -24,48 +22,18 @@ class SmartmeterController extends BaseController
     }
 
     public function actionIndex(){
-
-    }
-
-    public function actionStatus(){
+        BaseController::requireRole([Yii::$app->params['ROLE']['PEMILIK'], Yii::$app->params['ROLE']['PENYEWA']]);
         $unit = BuildingUnit::findOne(['unitid' => 1]);
         return $this->render('status', ['unit' => $unit]);
     }
 
     public function actionTopup(){
-
+        BaseController::requireRole([Yii::$app->params['ROLE']['PEMILIK'], Yii::$app->params['ROLE']['PENYEWA']]);
     }
 
     public function actionReport(){
         BaseController::requireRole([Yii::$app->params['ROLE']['PEMILIK']]);
 
-    }
-
-    public function actionAjax()
-    {
-        if (Yii::$app->request->isAjax) {
-            $unitid = Yii::$app->request->post()['unitid'];
-            $unit = BuildingUnit::find()->where(['unitid' => $unitid])->all();
-            $unitname = $unit[0]->nama;
-            $floorname = $unit[0]->floor->nama;
-            $towername = $unit[0]->tower->nama;
-
-            $result['total'] = count($unit);
-            if ($result['total'] > 0) {
-                $result['unitname'] = $unitname;
-                $result['floorname'] = $floorname;
-                $result['towername'] = $towername;
-                $electricity = Electricity::findOne(['unitid' => $unit[0]->unitid]);
-                if (count($electricity) > 0){
-                    $result['remaining'] = $electricity->saldo;
-                } else {
-                    $result['remaining'] = 0;
-                }
-            }
-            return json_encode($result);
-        } else {
-            throw new ForbiddenHttpException('You do not have permission to access this page.');
-        }
     }
 
 }
--- views/smartmeter/status.php
+++ views/smartmeter/status.php
@@ -1,18 +1,41 @@
 <?php
 
+use yii\helpers\Url;
+
 $this->title = 'Status';
 
 ?>
 
 <div class="row">
     <div class="col-md-12">
-        <div class="panel panel-primary" id="unit-information">
-            <div class="panel-heading with-border">
-                <h3 class="panel-title">Unit Information</h3>
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Unit Information</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                </div>
             </div>
-            <div class="panel-body">
-
-
+            <div class="box-body">
+                <p class="text-center"><i class="fa fa-home fa-5x"></i></p>
+                <h3 class="profile-username text-center text-bold" style="font-family: 'Agency FB';">107 P Permata hijau</h3>
+                <p class="text-muted text-center">Cassa de Belleza Tower</p>
+                <ul class="list-group list-group-unbordered">
+                    <li class="list-group-item">
+                        <b>Arus</b>
+                        <p class="pull-right">200 A</p>
+                    </li>
+                    <li class="list-group-item">
+                        <b>Tegangan</b>
+                        <p class="pull-right">22 V</p>
+                    </li>
+                    <li class="list-group-item">
+                        <b>Balance</b> <label class="label label-primary pull-right">144 kWh</label>
+                    </li>
+                </ul>
+                <a href="<?= Url::to(['smartmeter/topup']) ?>" class="btn btn-block btn-primary">
+                    <i class="fa fa-money"></i> Top Up
+                </a>
             </div>
         </div>
     </div>
